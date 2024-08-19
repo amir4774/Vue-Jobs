df1dd3d8999911165529a81f757a5b2cdddd939a
amir4774 <amirabbassi1382@gmail.com>
2024-08-19 19:28:53 +0330

    add primeicons

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -55,7 +55,7 @@ const truncateDescription = computed(() => {
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker"></i>
           {{ job.location }}
         </div>
         <a
