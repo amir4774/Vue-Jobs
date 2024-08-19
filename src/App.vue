0ff68e3945ab4ee913d0584eb7aca08dfeb80007
amir4774 <amirabbassi1382@gmail.com>
2024-08-19 19:23:11 +0330

    create Navbar, JobListings, HomeCard, and Hero

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
 <template>
-  <div><h1>Hello Amir</h1></div>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" />
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  bg: string;
+}>();
+</script>
+
+<template>
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
